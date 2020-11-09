3048a04696f3f65b344f2e310613d02afe1217b4
ffaizin <fajarfaizin77@gmail.com>
2020-11-10 00:35:35 +0700

    update database

--- application/controllers/PortalController.php
+++ application/controllers/PortalController.php
@@ -4,6 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class PortalController extends CI_Controller
 {
 
+  
     /**
      * Index Page for this controller.
      *
@@ -19,6 +20,12 @@ class PortalController extends CI_Controller
      * map to /index.php/welcome/<method_name>
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
         $login = true;
@@ -31,23 +38,54 @@ class PortalController extends CI_Controller
     }
     public function masuk()
     {
-        $login = true;
+        $login = false;
         if ($login) {
             redirect(base_url());
         }
+
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        
 
         $this->load->view('customer/navbar_customer');
         $this->load->view('usaha/masuk');
     }
     public function daftar()
     {
-        $login = true;
-        if ($login) {
-            redirect(base_url());
+
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('notelp', 'Telephone', 'required|trim');
+        $this->form_validation->set_rules('sandi', 'Sandi', 'required|trim|min_length[8]|matches[Konfirmasi]', [
+            'matches'=>'Password dont match.!',
+            'min_length'=>'Password too short.!'
+        ]);
+         $this->form_validation->set_rules('konfirmasi', 'Konfirmasi', 'required|trim|min_length[8]|matches[sandi]');
+
+        if($this->form_validation->run()==false){
+            $this->load->view('customer/navbar_customer');
+            $this->load->view('usaha/daftar');
+        }else{
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'upload_ktm' => $this->input->post('ktm'),
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_aktif' => 1,
+                'date_created' =>time(),
+                'photo_profile' => 'defautl.jpg',
+                'telephone' => $this->input->post('notelp'),
+
+
+            ];
+
+            $this->load->model->register($data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation your account has been created.!</div>');
+            
         }
 
-        $this->load->view('customer/navbar_customer');
-        $this->load->view('usaha/daftar');
+        
     }
     public function cari()
     {
--- application/views/usaha/daftar.php
+++ application/views/usaha/daftar.php
@@ -1,32 +1,34 @@
 <div class="row" style="font-family: Poppins, sans-serif;padding: 58px;padding-top: 0px;padding-bottom: 0px;padding-right: 58px;height: 408px;">
     <div class="col-12">
-        <form>
+         <form method="post" action="<?= base_url('masuk') ?>">
             <div class="form-row">
                 <div class="col d-flex d-lg-flex justify-content-center justify-content-lg-center" style="padding: 0px;margin-bottom: 20px;"><span class="text-center" style="font-size: 20px;color: #ff4141;font-weight: bold;font-family: Poppins, sans-serif;font-style: normal;">Daftar</span></div>
             </div>
             <div class="form-row d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="margin-bottom: 20px;">
-                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="form-control" type="text" placeholder="Nama" required=""></div>
+                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="form-control" type="text" name="name" placeholder="Nama" ></div>
             </div>
             <div class="form-row d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="margin-bottom: 20px;">
-                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="border rounded form-control" type="email" placeholder="Email" style="padding: 7px;" required=""></div>
+                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="border rounded form-control" name="email" type="email"  placeholder="Email" style="padding: 7px;" ></div>
             </div>
             <div class="form-row d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="margin-bottom: 20px;">
-                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="form-control" type="tel" placeholder="No-Telp" required=""></div>
+                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="form-control"  type="text" name="notelp" placeholder="No-Telp" ></div>
             </div>
             <div class="form-row d-flex justify-content-center align-items-center" style="margin-bottom: 20px;">
-                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="border rounded form-control" type="email" placeholder="Kata Sandi" style="padding: 7px;" required=""></div>
+                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="border rounded form-control" type="password" name="sandi" placeholder="Kata Sandi"  style="padding: 7px;" ></div>
             </div>
             <div class="form-row d-flex justify-content-center align-items-center" style="margin-bottom: 20px;">
-                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="border rounded form-control" type="email" placeholder="Konfirmasi Kata Sandi" style="padding: 7px;" required=""></div>
+                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input class="border rounded form-control" type="password" name="konfirmasi" placeholder="Konfirmasi Kata Sandi"  style="padding: 7px;" ></div>
             </div>
             <div class="form-row d-flex justify-content-center align-items-center" style="margin-bottom: 20px;">
                 <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><span>Foto KTM</span></div>
             </div>
             <div class="form-row d-flex justify-content-center align-items-center" style="margin-bottom: 20px;">
-                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input type="file" required="" accept="image/*"></div>
+                <div class="col-12 col-lg-6 d-flex d-xl-flex justify-content-center align-items-center justify-content-xl-center" style="padding: 0px;"><input name="ktm" type="file"  accept="image/*"></div>
             </div>
             <div class="form-row">
-                <div class="col d-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-lg-center justify-content-xl-center align-items-xl-center"><a class="btn btn-primary border rounded" role="button" style="width: 121px;background-color: rgb(255,65,65);" href="pencarian%20usaha.html">Daftar</a></div>
+                <div class="col d-flex d-lg-flex d-xl-flex justify-content-center align-items-center justify-content-lg-center justify-content-xl-center align-items-xl-center">
+                    <button class="btn btn-primary border rounded" type="submit" style="width: 121px;background-color: rgb(255,65,65);" href="pencarian%20usaha.html">Daftar</button>
+                </div>
             </div>
         </form>
     </div>
